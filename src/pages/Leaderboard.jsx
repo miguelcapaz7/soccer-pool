d67fdc336353a1288e3b9fa14f1fb560376f6b1c
miguelcapaz7 <miguelcapaz7@gmail.com>
2026-05-30 19:22:05 -0700

    Made leaderboard available after first matches

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -5,6 +5,8 @@ import LoadingSpinner from "../components/LoadingSpinner.jsx";
 
 const Leaderboard = () => {
   const { leaders, loading, error, columnHeaders, columns } = useLeaderboard();
+  const date = new Date("2026-06-11T21:00:00-07:00");
+  const leaderboardAvailable = new Date() >= date;
 
   return (
     <MainLayout title="Leaderboard">
@@ -12,6 +14,12 @@ const Leaderboard = () => {
         <LoadingSpinner />
       ) : error ? (
         <p style={{ color: "red" }}>{error}</p>
+      ) : !leaderboardAvailable ? (
+        <div className="card mt-3">
+          <div className="card-body text-center bg-light">
+            <p className="card-title mb-0 text-muted">Leaderboard will be available after the first matches are completed on June 11, 2026. </p>
+          </div>
+        </div>
       ) : leaders.length === 0 ? (
         <div className="card mt-3">
           <div className="card-body text-center bg-light">
